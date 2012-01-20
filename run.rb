c1d97459c2e5d505f2116d77c0b74432ceba871d
Robert Pearce <robertwaltonpearce@gmail.com>
2012-01-19 21:37:36 -0500

    debate mode

--- run.rb
+++ run.rb
@@ -1,14 +1,14 @@
 require './includes'
 require './candidate_tweets'
 
-cities = ['greenville', 'myrtle_beach', 'charleston', 'columbia']
+cities = ['charleston', 'columbia', 'greenville', 'myrtle_beach']
 
 loop do
   begin
     cities.each do |city|
       tweets = CandidateTweets.new(city)
       tweets.write_tally('2012-01-11 05:00:00')
-      queries = open('query2.txt').map { |line| line.gsub(/\n/,'') }
+      queries = open('query.txt').map { |line| line.gsub(/\n/,'') }
       queries.each {|query| tweets.get_tweets(query); sleep 30}
     end
   rescue
